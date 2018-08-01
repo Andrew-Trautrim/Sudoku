db5d6c64cf677dbfac3f73378fa1a167409b3391
Andrew Trautriim <atrautrim7@gmail.com>
2018-08-01 18:04:49 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this sudokulate file, choose Tools | Templates
- * and open the sudokulate in the editor.
- */
 
 /* 
  * File:   main.cpp
